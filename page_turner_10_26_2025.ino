a911c499056db5e706cadcc7394471013f112f66
Victoria <149974234+vicprzy@users.noreply.github.com>
2025-10-26 17:23:14 -0400

    Add files via upload
    Week 2 update: 
    - Started wiring this up to hardware
    - Incorporated a button that isn't the large dog buttons to see if code works
    - Added H-bridge for the motor, but that was unnecessary
    - Something was burning out, so we need to fix that

---
+++ page_turner_10_26_2025.ino
@@ -0,0 +1,56 @@
+// Names: Victoria Przydanek, Jishnuu Senthil Kumar, Digonto Chatterjee
+// Date: 10.26.2025
+// Description: Making a page turner for soft cover books/flimsy pages
+// Updates: We wanted to test out the hardware, so we included a button that isn't
+// the headphone jack but a simple button for implementing and testing.
+
+
+// importing libraries
+#include <Servo.h>
+
+// defining variables
+const int servoPin = 6;  // initializing servo
+Servo pageTurner;        // create servo object
+
+// H BRIDGE MOTOR DRIVER
+const int ENA_PIN = 9;  // Speed control
+const int IN1_PIN = 5;  // Direction control
+// *MIGHT CHANGE THE TIME = SPEED CONSTANT LATER*
+const int time = 500;   // how long servo waits for motor to run before it turns the page
+const int speed = 200;  // how fast the servo was moving
+
+const int buttonPin = 7;
+int buttonPress;  // set to False so the statement doesn't run continuously
+
+void setup() {
+  pageTurner.attach(servoPin);  // connect servo signal wire to Arduino Pin
+  pageTurner.write(0);          // get the servo to be in the neutral position
+
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+
+  pinMode(buttonPin, INPUT);  // declaring the button pin
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonPress = digitalRead(buttonPin);  // sets variable to button pin reading
+  // Serial.println(buttonPress); // debugs
+  if (buttonPress) {
+    Serial.println("I am working!");  // debugs
+    // M O T O R A C T I V A T I O N
+    digitalWrite(IN1_PIN, HIGH);  // lets the wheel start spinning
+    analogWrite(ENA_PIN, speed);  // lets us control how fast the motor is spinning
+    delay(time);                  // determines how long we want to have the wheel turn
+    digitalWrite(IN1_PIN, HIGH);  // wheel moved enough of page for servo to get under
+    analogWrite(ENA_PIN, 0);
+    // S E R V O M O V E M E N T
+    for (int i = 0; i < 180; i++) {  // this slows down the servo's run to prevent pages from breaking
+                                     // *MIGHT HAVE TO CHANGE LATER IF IT IS RUNNING TOO SLOW*
+      pageTurner.write(i);
+      delay(3);
+    }
+    // page has been turned
+    pageTurner.write(0);  // return servo to original position
+  }
+}
